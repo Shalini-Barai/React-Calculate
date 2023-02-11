8ca463eba3e20f55c4f1e976c786ea65ea543f62
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2023-02-11 15:40:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import "./App.css";
+const App = () => {
 
-function App() {
+	const [num1,setNum1]=useState();
+	const [num2,setNum2]=useState();
+	const [ans,setAns]=useState(0);
+	const [msg,setMsg]=useState("");
+	const [error, setError] = useState(false);
+	const [color, setColor] = useState('#fff');
+
+   const handleChange=(event)=>{
+         const { name, value }=event.target;
+		 if(name==="num1") setNum1(parseInt(value));
+		 else if(name==="num2") setNum2(parseInt(value));
+		 event.preventDefault();
+         if (!value){
+          setError(true);
+        }	 
+   }
+    const handleAction=(action)=>{
+           if(action==="+") {
+			setAns(num1+num2)  
+			setColor("green")
+			setMsg("Success:Your result is shown above!")
+			setError(false)
+	        }
+		   else if(action==="-") {
+			setAns(num1-num2)
+			setMsg("Success:Your result is shown above!")
+			setError(false)
+	        }
+		   else if(action==="*") {
+			setAns(num1*num2)
+			setMsg("Success:Your result is shown above!")
+			setError(false)
+			}
+		   else if(action==="/") {
+			setAns(num1/num2);
+			setMsg("Success:Your result is shown above!")
+			setError(false)
+		   }
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	<div className="main">
+	  <h1>React Calculator</h1><br></br>
+	  <input type="number" value={num1} name="num1" onChange={(event)=>handleChange(event)}></input>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  <input type="number" value={num2} name="num2" onChange={(event)=>handleChange(event)}></input><br></br><br></br>
+	  <div className='arith'>
+	  <button className='btn' onClick={()=>{handleAction("+")}}>+</button>
+	  <button className='btn' onClick={()=>{handleAction("-")}}>-</button>
+	  <button className='btn' onClick={()=>{handleAction("*")}}>*</button>
+	  <button className='btn' onClick={()=>{handleAction("/")}}>/</button>
+	  <h3 style={{ color }}>Result= {ans} <br></br>{msg} </h3>
+	  {error && <p>Error : Num1 and Num2 can not be empty</p>}
+</div>
+	</div>
+  )
 }
 
 export default App;
---
+++ src/todoList.js
@@ -0,0 +1,9 @@
+function TodoList(props){
+    const {todoList,name}=props;
+    return (
+        <div><h1>Todo List Component</h1></div>
+
+
+    )
+}
+export default TodoList;
